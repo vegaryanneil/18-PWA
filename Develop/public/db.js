a0d0ba46f0e81a25a42ee08035649f8dc2cf6e3d
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-11 23:32:44 -0400

    add fx to access pending object

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -5,12 +5,12 @@ const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(e) {
     // create "pending" obj and set autoIncrement
-    const db = event.target.result;
+    const db = e.target.result;
     db.createObjectStore("pending", { autoIncrement: true});
 };
 
 request.onsuccess = function(e) {
-    db = event.target.result;
+    db = e.target.result;
     if (navigator.onLine) {
         checkDatabase();
     }
@@ -28,3 +28,27 @@ function checkDatabase() {
     // pull all information from store set it to a variable
     const getAll = store.getAll();
 }
+
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        // if successful, open a transaction on your pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+        // access your pending object store
+        const store = transaction.objectStore("pending");
+        // clear all items in your store
+        store.clear();
+      });
+    }
+  };
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
