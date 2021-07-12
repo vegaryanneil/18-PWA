17666d3242a3e1d78721620e601a3a0d7127f39c
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-11 22:46:57 -0400

    added check database fx

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -19,3 +19,12 @@ request.onsuccess = function(e) {
 request.onerror = function(e) {
     console.log("Error:" + e.target.errorCode);
 };
+
+function checkDatabase() {
+    // open transaction on current db
+    let transaction = db.transaction(['pending'], 'readwrite')
+    // access current object store
+    const store = transaction.objectStore("pending")
+    // pull all information from store set it to a variable
+    const getAll = store.getAll();
+}
