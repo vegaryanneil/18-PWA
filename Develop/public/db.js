3265dff7f892862316254d94888fe9b815c7ab2b
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-11 22:37:18 -0400

    Added fx to check error

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -3,8 +3,19 @@ let db;
 // create db request for budget database
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function() {
+request.onupgradeneeded = function(e) {
     // create "pending" obj and set autoIncrement
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true});
+};
+
+request.onsuccess = function(e) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+}
+
+request.onerror = function(e) {
+    console.log("Error:" + e.target.errorCode);
 };
