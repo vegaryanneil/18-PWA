3e9498086acc291513f103a19c68f17947182ab2
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-15 08:59:20 -0400

    Added const port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3001;
+//This is important here !!!!!! we need to add this process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
